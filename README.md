a1775ac0521aa02637c1af75159d182e15fa0c77
Unknown <samuel.gonzalez9@upr.edu>
2020-09-27 19:48:43 -0400

    Fully Implemented Instruction and Data RAM
    Fixed Instruction and Data RAM implementation. Adjusted timing for accurate printing of information. Updated readme with info about access modes. Implemented and passed test cases specified on the instructions for phase 1.

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Computer_Architecture_Pipeline
 Computer Architecture UPRM Course Project (Fall 2020)
 
+Added Mode channel for access type when reading:
+    00: Read Byte
+    01: Read Halfword
+    10: Read Word
+    11: Read Doubleword
+
 To execute:
     Navigate to project directory on a terminal
     Compile with: 
--- root/PF1_Gonzalez_Ortiz_Samuel_ram.v
+++ root/PF1_Gonzalez_Ortiz_Samuel_ram.v
@@ -1,28 +1,28 @@
-module instRAM256x8 (output reg [31:0] DataOut, input Enable, 
-input[31:0] Address, input [7:0] DataIn);
+module instRAM256x8 (output reg [31:0] DataOut, input Enable, input[31:0] Address);
     reg[7:0] Mem[0:255];
     always@(Enable) 
         if(Enable)
-            Mem[Address] = DataIn;  //write  
+            DataOut = Mem[Address];
 endmodule
 
 
 module dataRAM256x8 (output reg [31:0] DataOut, input Enable, ReadWrite,
-input[31:0] Address, input [7:0] DataIn, input [1:0] Mode);
+input[31:0] Address, input [31:0] DataIn, input [1:0] Mode);
     reg[7:0] Mem[0:255];
+    reg[31:0] temp;
     always@(Enable, ReadWrite)
         if(Enable)
-            if(ReadWrite)//read
+            if(!ReadWrite)//read
                 case(Mode)
-                    2'b00:  //Access Byte
+                    2'b00:  //Read Byte
                         DataOut = Mem[Address];
-                    2'b01:  //Access Halfword: 2 bytes
+                    2'b01:  //Read Halfword: 2 bytes
                         begin
                             DataOut = Mem[Address];
                             DataOut = DataOut << 8;
                             DataOut = DataOut + Mem[Address+1];
                         end
-                    2'b10:  //Access Word: 4 bytes
+                    2'b10:  //Read Word: 4 bytes
                         begin
                             DataOut = Mem[Address];
                             DataOut = DataOut << 8;
@@ -32,7 +32,7 @@ input[31:0] Address, input [7:0] DataIn, input [1:0] Mode);
                             DataOut = DataOut << 8;
                             DataOut = DataOut + Mem[Address+3];
                         end
-                    2'b11:  //Access Doubleword: 2 * 4 bytes
+                    2'b11:  //Read Doubleword: 2 * 4 bytes
                         begin
                             //first word
                             DataOut = Mem[Address];
@@ -43,7 +43,7 @@ input[31:0] Address, input [7:0] DataIn, input [1:0] Mode);
                             DataOut = DataOut << 8;
                             DataOut = DataOut + Mem[Address+3];
                             //second word
-                            #3 DataOut = Mem[Address+4];
+                            #6 DataOut = Mem[Address+4];
                             DataOut = DataOut << 8;
                             DataOut = DataOut + Mem[Address+5];
                             DataOut = DataOut << 8;
@@ -52,231 +52,240 @@ input[31:0] Address, input [7:0] DataIn, input [1:0] Mode);
                             DataOut = DataOut + Mem[Address+7];
                         end
                 endcase
-        else//write
-            Mem[Address] = DataIn;
+            else//write
+                case(Mode)
+                    2'b00:  //Write Byte
+                        Mem[Address] = DataIn;
+                    2'b01:  //Write Halfword: 2 bytes
+                        begin
+                            temp = DataIn & 32'hff00;
+                            temp = temp >> 8;
+                            Mem[Address] = temp;
+                            temp = DataIn & 32'hff;
+                            Mem[Address+1] = temp;
+                        end
+                    2'b10:  //Write Word: 4 bytes
+                        begin
+                            temp = DataIn & 32'hff000000;
+                            temp = temp >> 24;
+                            Mem[Address] = temp;
+                            temp = DataIn & 32'hff0000;
+                            temp = temp >> 16;
+                            Mem[Address+1] = temp;
+                            temp = DataIn & 32'hff00;
+                            temp = temp >> 8;
+                            Mem[Address+2] = temp;
+                            temp = DataIn & 32'hff;
+                            Mem[Address+3] = temp;
+                        end
+                    2'b11:  //Write Doubleword: 2 * 4 bytes
+                        begin
+                            //do nothing
+                        end
+                endcase
 endmodule
 
 
 module prechargeInstRAM;
-integer inFile, code;
-    reg Enable;
-    reg [7:0] DataIn;
-    reg [31:0] Address;
-    wire [31:0] DataOut;
-    instRAM256x8 ramI(DataOut, Enable, Address, DataIn);
+//////////////
+//Pre-charge//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    integer I_inFile, I_code;
+    reg I_Enable;
+    reg [7:0] data;
+    reg [31:0] I_Address;
+    wire [31:0] I_DataOut;
+    instRAM256x8 ramI(I_DataOut, I_Enable, I_Address);
     initial
         begin
-            inFile = $fopen("PF1_Gonzalez_Ortiz_Samuel_ramintr.txt","r");
-            Address = 32'b0;
+            I_inFile = $fopen("PF1_Gonzalez_Ortiz_Samuel_ramintr.txt","r");
+            I_Address = 32'b0;
             $display("");
             $display("Instruction RAM was precharged with the following data:");
             $display("        Address   Data");
-            #10; //give time for initialization on first enable cycle
-            while(!$feof(inFile))
+            while(!$feof(I_inFile))
                 begin
-                    #1 code = $fscanf(inFile, "%b", DataIn); //pass read value through DataIn
-                    #7 $display("%d        %b", Address, ramI.Mem[Address]); //print value on Mem to confire that the value was stored
-                    #2 Address = Address + 1; 
+                    #1 I_code = $fscanf(I_inFile, "%b", data); //pass read value through DataIn
+                    ramI.Mem[I_Address] = data;
+                    #7 $display("%d        %b", I_Address, data);
+                    #2 I_Address = I_Address + 1; 
                 end
-            $fclose(inFile);
+            $fclose(I_inFile);
         end
     initial
         begin
-            Enable = 1'b0;
-            repeat (40)
+            I_Enable = 1'b0;
+            repeat (16)
                 begin
-                    #5 Enable = 1'b1;
-                    #5 Enable = 1'b0;
+                    #5 I_Enable = 1'b1;
+                    #5 I_Enable = 1'b0;
                 end
-            $finish;
-        end        
-endmodule
+        end  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-module prechargeDataRAM;
-    integer inFile, code;
-    reg Enable, ReadWrite;
-    reg[1:0] Mode;
-    reg [7:0] DataIn;
-    reg [31:0] Address;
-    wire [31:0] DataOut;
-    dataRAM256x8 ramD(DataOut, Enable, ReadWrite, Address, DataIn, Mode);
-    initial
-        begin
-            #200; //give time for Instruction RAM Initialization to end
-            inFile = $fopen("PF1_Gonzalez_Ortiz_Samuel_ramdata.txt","r");
-            Address = 32'b0;
-            $display("");
-            $display("Data RAM was precharged with the following data:");
-            $display("        Address   Data");
-            #10; //give time for initialization on first enable cycle
-            while(!$feof(inFile))
-                begin
-                    #1 code = $fscanf(inFile, "%b", DataIn);
-                    #7 $display("%d        %b", Address, ramD.Mem[Address]);
-                    #2 Address = Address + 1; 
-                end
-            $fclose(inFile);
-        end
-    initial
-        begin
-            #200; //give time for Instruction RAM Initialization to end
-            Enable = 1'b0;
-            ReadWrite = 1'b0;
-            repeat (20)
-                begin
-                    #5 Enable = 1'b1;
-                    #5 Enable = 1'b0;
-                end
-            $finish;
-        end        
-endmodule
-
-module testInstRAM;
-endmodule
-
-module testDataRAM;
+////////
+//Test//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     initial
         begin
             #400;
             $display("");
-            $display("Testing Data RAM:");
+            $display("Testing Instruction RAM:");
             $display("        Address   Data");
-            $display("%d        %b     %h        %h", Address, ReadWrite, DataIn, DataOut);
-            Enable = 1'b0;
-            ReadWrite = 1'b1;
-            prechargeDataRAM.ramD.Mode = 2'b10;
-            #1 Address = 32'b0;
-            repeat (16/Mode)
-                begin
-                    #5 Enable = 1'b1;
-                    #5 Enable = 1'b0;
-                    Address = Address+Mode+1;
-                end
-            $finish;
-        end
-
-        always @ (posedge Enable)
-            begin
-                #1;
-                case(Mode)
-                    2'b11:  //Access Doubleword: 2 * 4 bytes
-                        begin
-                            DataOutprev <= DataOut;
-                            #4;
-                            $display("%d        %b     %h        %h %h", Address, ReadWrite, DataIn, DataOutprev, DataOut);
-                        end
-                    default:
-                        $display("%d        %b     %h        %h", Address, ReadWrite, DataIn, DataOut);
-                endcase
-            end
-
-        //read word (Mode 10) from 0,4,8,12
-        //read Mode 00 from 0, Mode o1 from 2, Mode 01 from 4, Mode 10 from 8
-        //read Mode 10 from 4,8
-        //$display("");
-        //$display("Data RAM Testing:");
-        //$display("        Address   R/W   Data In   Data Out");
-        //$display("%d        %b     %h        %h", Address, ReadWrite, DataIn, DataOut)
-endmodule
-
-/*
-module prechargeDataRAMref;
-    integer inFile, outFile, code;
-    reg Enable, ReadWrite;
-    reg [1:0] Mode;
-    reg [7:0] data;
-    reg [7:0] DataIn;
-    reg [31:0] Address;
-    reg [31:0] DataOutprev;    //for use with doubleword
-    wire [31:0] DataOut;
-    dataRAM256x8 ramD(DataOut, Enable, ReadWrite, Address, DataIn, Mode);
-    initial
-        begin
-            inFile = $fopen("PF1_Gonzalez_Ortiz_Samuel_ramdata.txt","r");
-            Address = 32'b0;
-            while(!$feof(inFile))
-                begin
-                   code = $fscanf(inFile, "%b", data);
-                   ramD.Mem[Address] = data;
-                   Address = Address + 1; 
-                end
-            $fclose(inFile);
-        end
-    initial
-        begin
-            outFile = $fopen("PF1_Gonzalez_Ortiz_Samuel_output.txt", "w");
-            $fdisplay(outFile, "Data RAM precharged with the following:");
-            $fdisplay(outFile, "        Address   R/W   Data In   Data Out");
-            Enable = 1'b0;
-            ReadWrite = 1'b1;
-            Mode = 2'b10;
-            #1 Address = 32'b0;
-            repeat (16/Mode)
-                begin
-                    #5 Enable = 1'b1;
-                    #5 Enable = 1'b0;
-                    Address = Address+Mode;
-                end
-            $finish;
-        end
-    always @ (posedge Enable)
-        begin
-            #1;
-            case(Mode)
-                2'b11:  //Access Doubleword: 2 * 4 bytes
-                    begin
-                        DataOutprev <= DataOut;
-                        #4;
-                        $fdisplay(outFile, "%d        %b     %h        %h %h", Address, ReadWrite, DataIn, DataOutprev, DataOut);
-                    end
-                default:
-                    $fdisplay(outFile, "%d        %b     %h        %h", Address, ReadWrite, DataIn, DataOut);
-            endcase
-        end
-endmodule
-*/
-
-/*
-module prechargeInstRAMref;
-    integer inFile, outFile, code;
-    reg Enable;
-    reg [7:0] data;
-    reg [7:0] DataIn;
-    reg [31:0] Address;
-    wire [31:0] DataOut;
-    instRAM256x8 ramI(DataOut, Enable, Address, DataIn);
-    initial
-        begin
-            inFile = $fopen("PF1_Gonzalez_Ortiz_Samuel_ramintr.txt","r");
-            Address = 32'b0;
-            while(!$feof(inFile))
-                begin
-                   code = $fscanf(inFile, "%b", data);
-                   ramI.Mem[Address] = data;
-                   Address = Address + 1; 
-                end
-            $fclose(inFile);
-        end
-    initial
-        begin
-            outFile = $fopen("PF1_Gonzalez_Ortiz_Samuel_output.txt", "w");
-            $fdisplay(outFile, "Address   Data Out");
-            Enable = 1'b0;
-            Address = 32'b0;
+            I_Enable = 1'b0;
+            I_Address = 32'b0;
             repeat (16)
                 begin
-                    #5 Enable = 1'b1;
-                    #5 Enable = 1'b0;
-                    Address = Address+1;
+                    #5 I_Enable = 1'b1;
+                    #5 I_Enable = 1'b0;
+                    $display("%d        %h", I_Address, I_DataOut);
+                    I_Address = I_Address + 1;
                 end
-            $finish;
-            
         end
-    always @ (posedge Enable)
-        begin
-            #1;
-            $fdisplay(outFile, "%d   %h", Address, DataOut);
-        end
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////      
 endmodule
-*/
+
+
+module prechargeDataRAM;
+//////////////
+//Pre-charge//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    integer D_inFile, D_code;
+    reg D_Enable, D_ReadWrite;
+    reg[1:0] D_Mode;
+    reg [31:0] D_DataIn;
+    reg [31:0] D_DataOutprev; //stores most significant bit data for doublewords
+    reg [31:0] D_Address;
+    wire [31:0] D_DataOut;
+    dataRAM256x8 ramD(D_DataOut, D_Enable, D_ReadWrite, D_Address, D_DataIn, D_Mode);
+    initial
+        begin
+            #200; //wait for Instruction RAM Initialization to end (used only for precharge prints)
+            D_inFile = $fopen("PF1_Gonzalez_Ortiz_Samuel_ramdata.txt","r");
+            D_Address = 32'b0;
+            $display("");
+            $display("Data RAM was precharged with the following data:");
+            $display("        Address   Data");
+            while(!$feof(D_inFile))
+                begin
+                    #1 D_code = $fscanf(D_inFile, "%b", D_DataIn);
+                    #7 $display("%d        %b", D_Address, ramD.Mem[D_Address]);
+                    #2 D_Address = D_Address + 1; 
+                end
+            $fclose(D_inFile);
+        end
+    initial
+        begin
+            #200; //wait for Instruction RAM Initialization to end (used only for precharge prints)
+            D_Enable = 1'b0;
+            D_Mode = 2'b0;
+            D_ReadWrite = 1'b1;
+            repeat (16)
+                begin
+                    #5 D_Enable = 1'b1;
+                    #5 D_Enable = 1'b0;
+                end
+        end  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////
+//Test//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    initial //test case 1
+        begin 
+            #600;
+            $display("");
+            $display("Testing Data RAM (case 1):");
+            $display("        Address   R/W   Data In   Data Out");
+
+            D_Enable = 1'b0;
+            D_ReadWrite = 1'b0;
+            D_Mode = 2'b10;
+            D_Address = 32'b0;
+            repeat (4)
+                begin
+                    #5 D_Enable = 1'b1;
+                    #5 D_Enable = 1'b0;
+                    $display("%d        %b     %h        %h", D_Address, D_ReadWrite, D_DataIn, D_DataOut);
+                    D_Address = D_Address + 4;
+                end
+        end
+
+    initial //test case 2
+        begin
+            #650
+            $display("");
+            $display("Testing Data RAM (case 2):");
+            $display("        Address   R/W   Data In   Data Out");
+            D_Enable = 1'b0;
+            D_ReadWrite = 1'b0;
+            D_Mode = 2'b00;
+            D_Address = 32'b0;
+                #5 D_Enable = 1'b1;
+                #5 D_Enable = 1'b0;
+                $display("%d        %b     %h        %h", D_Address, D_ReadWrite, D_DataIn, D_DataOut);
+            D_Mode = 2'b01;
+            D_Address = 32'b10;
+                #5 D_Enable = 1'b1;
+                #5 D_Enable = 1'b0;
+                $display("%d        %b     %h        %h", D_Address, D_ReadWrite, D_DataIn, D_DataOut);
+            D_Address = 32'b100;
+                #5 D_Enable = 1'b1;
+                #5 D_Enable = 1'b0;
+                $display("%d        %b     %h        %h", D_Address, D_ReadWrite, D_DataIn, D_DataOut);
+        end
+
+    initial //test case 3
+        begin
+            #700
+            $display("");
+            $display("Testing Data RAM (case 3):");
+            $display("        Address   R/W   Data In   Data Out");
+            D_Enable = 1'b0;
+            D_ReadWrite = 1'b1;
+            D_Mode = 2'b00;
+            D_Address = 32'b0;
+                D_DataIn = 32'haa;
+                #5 D_Enable = 1'b1;
+                #5 D_Enable = 1'b0;
+                $display("%d        %b     %h        %h", D_Address, D_ReadWrite, D_DataIn, D_DataOut);
+            D_Mode = 2'b01;
+            D_Address = 32'b10;
+                D_DataIn = 32'haaaa;
+                #5 D_Enable = 1'b1;
+                #5 D_Enable = 1'b0;
+                $display("%d        %b     %h        %h", D_Address, D_ReadWrite, D_DataIn, D_DataOut);
+            D_Mode = 2'b01;
+            D_Address = 32'b100;
+                D_DataIn = 32'haaaa;
+                #5 D_Enable = 1'b1;
+                #5 D_Enable = 1'b0;
+                $display("%d        %b     %h        %h", D_Address, D_ReadWrite, D_DataIn, D_DataOut);
+            D_Mode = 2'b10;
+            D_Address = 32'b1000;
+                D_DataIn = 32'haaaaaaaa;
+                #5 D_Enable = 1'b1;
+                #5 D_Enable = 1'b0;
+                $display("%d        %b     %h        %h", D_Address, D_ReadWrite, D_DataIn, D_DataOut);
+        end
+
+    initial //test case 4
+        begin
+            #750
+            $display("");
+            $display("Testing Data RAM (case 4):");
+            $display("        Address   R/W   Data In   Data Out");
+            D_Enable = 1'b0;
+            D_ReadWrite = 1'b0;
+            D_Mode = 2'b10;
+            D_Address = 32'b100;
+                #5 D_Enable = 1'b1;
+                #5 D_Enable = 1'b0;
+                $display("%d        %b     %h        %h", D_Address, D_ReadWrite, D_DataIn, D_DataOut);
+            D_Address = 32'b1000;
+                #5 D_Enable = 1'b1;
+                #5 D_Enable = 1'b0;
+                $display("%d        %b     %h        %h", D_Address, D_ReadWrite, D_DataIn, D_DataOut);
+        end
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+endmodule
